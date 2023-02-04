d349405d079e88b132569f05419c4a2a55b4ee04
Kudryavtsev Kirill <rtutiui@mail.ru>
2023-02-04 20:57:16 +0300

    add issue for b task

--- solutions/b-summa-k-oplate/b-summa-k-oplate.go
+++ solutions/b-summa-k-oplate/b-summa-k-oplate.go
@@ -6,19 +6,27 @@ import (
 	"os"
 )
 
-func sum() {
+func summaKoplate() {
 	in := bufio.NewReader(os.Stdin)
-	res := bufio.NewWriter(os.Stdout)
-	defer res.Flush()
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
 
-	var n map[int]int
+	var testCount int
+	fmt.Fscan(in, &testCount)
 
-	fmt.Fscan(in, &n)
-
-	for i := 0; i < len(n); i++ {
+	for i := 0; i < testCount; i++ {
+		var productCount, sum int
+		var products = make(map[int]int)
+		fmt.Fscan(in, &productCount)
+		for i := 0; i < productCount; i++ {
+			var productPrice int
+			fmt.Fscan(in, &productPrice)
+			products[productPrice]++
+		}
+		for k, v := range products {
+			sum += (k * ((v / 3 * 2) + v%3))
+		}
+		fmt.Fprintln(out, sum)
 	}
-}
 
-func testing_sum(a int, b int, res []int) int {
-	return 12
 }
