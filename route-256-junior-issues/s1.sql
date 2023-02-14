9ceb30270ccc3edad1221d821fb915d1659092cd
Kudryavtsev Kirill <rtutiui@mail.ru>
2023-02-14 23:46:13 +0300

    add s1 sql issue

---
+++ route-256-junior-issues/s1.sql
@@ -0,0 +1,11 @@
+select u.id, u.name,
+       (select count(distinct c.id) from submissions s
+                    inner join problems p on (p.id = s.problem_id)
+                    inner join contests c on (c.id = p.contest_id)
+               where s.user_id = u.id and s.success)        as solved_at_least_one_contest_count,
+       (select count(distinct c.id) from submissions s
+                    inner join problems p on (p.id = s.problem_id)
+                    inner join contests c on (c.id = p.contest_id)
+               where s.user_id = u.id)                      as take_part_contest_count
+from users u
+order by solved_at_least_one_contest_count desc, take_part_contest_count desc, u.id
